f4a1f87ad083bd8b12fc8142f75305c05b9c2aef
ArtemPanko123 <104429669+ArtemPanko123@users.noreply.github.com>
2024-07-16 18:46:56 +0300

    locale pickers

--- src/App.js
+++ src/App.js
@@ -1,35 +1,53 @@
-import React from 'react';
+import React, {useState} from 'react';
 import SpeechRecognition, { useSpeechRecognition } from 'react-speech-recognition';
 
+const locales =[
+    {name: 'English', value: 'en_US'},
+    {name: 'Thai', value: 'th_TH'},
+    {name: 'Ukrainian', value: 'uk_UA'},
+    {name: 'Czech', value: 'cs_CZ'},
+    {name: 'Greek', value: 'el_GR'},
+    {name: 'Spanish', value: 'es_ES'},
+]
+
 const Dictaphone = () => {
-  const {
-    transcript,
-    listening,
-    resetTranscript,
-    browserSupportsSpeechRecognition
-  } = useSpeechRecognition();
+    const [locale, setLocale] = useState(locales[0].value);
 
-  if (!browserSupportsSpeechRecognition) {
-    return <span>Browser doesn't support speech recognition.</span>;
-  }
+    const {
+        transcript,
+        listening,
+        resetTranscript,
+        browserSupportsSpeechRecognition
+    } = useSpeechRecognition();
 
-  return (
-      <div>
-        <p>Microphone: {listening ? 'on' : 'off'}</p>
-        <button onClick={() => {SpeechRecognition.startListening({
-            language: 'en-US'
-        })}}>Start</button>
-        <button onClick={SpeechRecognition.stopListening}>Stop</button>
-        <button onClick={resetTranscript}>Reset</button>
-        <p>{transcript}</p>
-      </div>
-  );
+    if (!browserSupportsSpeechRecognition) {
+        return <span>Browser doesn't support speech recognition.</span>;
+    }
+
+    const handleLocaleChange = (event) => {
+        setLocale(event.target.value);
+    }
+
+    return (
+        <div>
+            <p>Microphone: {listening ? 'on' : 'off'}</p>
+            <select value={locale} onChange={handleLocaleChange}>
+                {locales.map((locale, index) => (
+                    <option key={index} value={locale.value}>{locale.name}</option>
+                ))}
+            </select>
+            <button onClick={() => {SpeechRecognition.startListening({ language: locale })}}>Start</button>
+            <button onClick={SpeechRecognition.stopListening}>Stop</button>
+            <button onClick={resetTranscript}>Reset</button>
+            <p>{transcript}</p>
+        </div>
+    );
 };
 
 function App() {
-  return (
-   <Dictaphone/>
-  );
+    return (
+        <Dictaphone/>
+    );
 }
 
 export default App;
